763069b6202d068d3823a15cd5c24669a032b0a8
RyoDagg <mannai.abdlkader@gmail.com>
2025-10-21 12:23:15 +0100

    Integrate Mailtrap mail service

---
+++ server/src/mail/mail.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { MailtrapClient } from 'mailtrap';
+
+@Injectable()
+export class MailService {
+  async sendMail({
+    to,
+    subject,
+    body,
+    fromEmail = 'no-reply@livora.tn',
+    fromName = 'Livora Team',
+  }: {
+    to: string;
+    subject: string;
+    body: string;
+    fromEmail?: string;
+    fromName?: string;
+  }) {
+    if (!process.env.MAILTRAP_TOKEN)
+      throw new Error('MAILTRAP_TOKEN is missing in environment variables.');
+
+    const client = new MailtrapClient({
+      token: process.env.MAILTRAP_TOKEN!,
+    });
+
+    try {
+      await client.send({
+        from: {
+          email: fromEmail,
+          name: fromName,
+        },
+        to: [{ email: to }],
+        subject,
+        text: body,
+      });
+    } catch (error) {
+      console.error(`Failed to send email to ${to}:`, error);
+      throw new Error('Failed to send email.');
+    }
+  }
+}
