feee713e0c8338891d72bf804c3637a913cf9f39
RyoDagg <mannai.abdlkader@gmail.com>
2025-10-27 08:17:04 +0100

    fix: update cookie settings for production environment

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -24,7 +24,8 @@ export class AuthController {
 
       res.cookie('jwt_token', access_token, {
         httpOnly: true,
-        sameSite: 'lax',
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       });
 
       return { ok: true, user };
